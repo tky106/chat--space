0fc6204682a6e287fac492420469964d04661bc3
tky106 <tktktky106@gmail.com>
2019-10-03 11:47:57 +0900

    implement group function

--- Gemfile
+++ Gemfile
@@ -57,3 +57,5 @@ gem 'haml-rails'
 gem "font-awesome-rails"
 
 gem "devise"
+
+gem 'erb2haml'
--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,12 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
-    config.i18n.default_locale = :ja
+    config.generators do |g|
+      config.i18n.default_locale = :ja
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+    end
   end
 end
