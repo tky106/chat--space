bb0d7a2b6c3f61108f0dfdf701ae828252eb3cf6
tky106 <tktktky106@gmail.com>
2019-10-15 14:17:35 +0900

    erase useless description

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function() {
   var search_list = $("#chat-group-users")
   function appendUser(user) {
-    // console.log(user)
+    
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</div>
@@ -19,7 +19,7 @@ $(document).on('turbolinks:load', function() {
   }
   $("#user-search-field").on("keyup", function(e) {
     e.preventDefault();
-    //var input = $("#user-seach-result").val();
+   
     var input = $("#user-search-field").val(); 
     
     if(input.length == 0) {
