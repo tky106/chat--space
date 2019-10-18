c08d2504403a18dffb03102621e213cc4f9d2d12
tky106 <tktktky106@gmail.com>
2019-10-18 16:07:39 +0900

    modify some points

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -44,37 +44,9 @@ $(document).on('turbolinks:load', function() {
       $(".contents__right-content--right-box").animate({scrollTop: $(".contents__right-content--right-box")[0].scrollHeight});
     })
     .fail(function() {
-      console.log('error');
+      alert('error');
     });
   };
-  function buildHTML(message) {
-
-    var content =
-      (message.content && message.image.url) ? message.content
-    : (message.content) ? message.content
-    : (message.image.url) ? ""
-      
-    var image =
-      (message.content && message.image.url) ? message.image
-    : (message.content) ? message.image
-    : (message.image.url) ? ""
-      
-
-      var html = `<div class="contents__right-content--right-box__test-date" data-id="${message.id}">
-                    <div class="contents__right-content--right-box__test-date--test">
-                        ${message.user_name}       
-                    </div>
-                    <div class="contents__right-content--right-box__test-date--date">
-                      ${message.date}
-                    </div>
-                  </div>
-                  <div class="contents__right-content--right-box__hello" data-id="${message.id}">
-                    ${content}
-                  </div>
-                  <div><img src="${image}"></div>`
- 
-    return html;
-  }
   
   function scrollBottom(){
     var target = $('.contents__right-content--right-box__test-date, .contents__right-content--right-box__hello').last();
@@ -97,7 +69,7 @@ $(document).on('turbolinks:load', function() {
       contentType: false
     })
     .done(function(data) {
-      var html = buildHTML(data);
+      var html = buildMessageHTML(data);
       $(".contents__right-content--right-box").append(html);
       $("#new_message")[0].reset();
       scrollBottom();
