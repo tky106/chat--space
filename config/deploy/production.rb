d26950da2a23904e546f73f52257245492f1ee09
tky106 <tktktky106@gmail.com>
2019-10-19 19:36:38 +0900

    implement auto updating function

---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '3.115.97.127', user: 'ec2-user', roles: %w{app db web}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
+  # Apache or  already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
