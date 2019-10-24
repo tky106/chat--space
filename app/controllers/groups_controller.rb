b26c3cafce37c3fc2055c255eb5a1b112ee1c362
tky106 <tktktky106@gmail.com>
2019-10-24 11:59:02 +0900

    Update groups_controller.rb

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,8 +23,9 @@ class GroupsController < ApplicationController
   end
 
   def update
-
+    
     if @group.update(group_params)
+      @group.users << current_user
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
        render :edit 
